9b5c8eca23b067f6062acac1c024e4eca4e4e731
Kevin Liu <kevin91liu@gmail.com>
2017-03-05 19:28:50 -0500

    fix indentation, fixed a bug

--- helpers.py
+++ helpers.py
@@ -6,26 +6,26 @@
 #since TCP is a stream-based protocol and we need a message-based protocol (to figure out in the byte-stream the
 #boundaries when one message ends and another begins), we send a 4-byte int that specifies the length of the
 #message. we then read that many bytes from the socket.
-
+import struct
 
 def send_message(socket, message):
     message = struct.pack('>I', len(message)) + message
     socket.sendall(message)
 
 def recv_message(socket):
-    packedMessageLength = recvall(socket, 4)
+    packedMessageLength = recvData(socket, 4)
     if not packedMessageLength: #then the socket was closed
         return None
     messageLength = struct.unpack('>I', packedMessageLength)[0]
     # Read the message data
-    return recvall(socket, messageLength)
+    return recvData(socket, messageLength)
 
 #reads n bytes of data from the socket
 def recvData(socket, n):
     data = ''
     while len(data) < n:
         chunk = socket.recv(n - len(data))
-        if not packet: #then the socket was closed
+        if not chunk: #then the socket was closed
             return None
         data += chunk
-   return data
+    return data
--- server.py
+++ server.py
@@ -23,34 +23,34 @@ import os
 
 #if you run "python server.py 1555 auth/server.crt auth/server.key auth/client.crt", then sys.argv has 5 arguments: server.py is the 1st, 1555 is the 2nd, auth/server.crt is the 3rd, auth/server.key is the 4th, and auth/client.crt is the 5th
 if(len(sys.argv) != 5):
-	print "Invalid number of arguments. Invoke the server using: python server.py <port> <server certificate> <server private key> <client certificate>, where <port> is a port number in the inclusive range [1024, 65535], <server certificate> is the server's certificate file (for example, auth/server.crt), <server private key> is the server's RSA private key file (for example, auth/server.key), and <client certificate> is the client's certificate file (for example, auth/client.crt)"
+  print "Invalid number of arguments. Invoke the server using: python server.py <port> <server certificate> <server private key> <client certificate>, where <port> is a port number in the inclusive range [1024, 65535], <server certificate> is the server's certificate file (for example, auth/server.crt), <server private key> is the server's RSA private key file (for example, auth/server.key), and <client certificate> is the client's certificate file (for example, auth/client.crt)"
 
 if(not sys.argv[1].isdigit()):
-	print 'Port number must contain only digits 0-9'
-	exit()
+  print 'Port number must contain only digits 0-9'
+  exit()
 serverPort = int(sys.argv[1])
 #65535 is the max valid port number. port numbers below 1024 don't work because of some sort of permissioning
 #thing -- i think it's because many of those port numbers are reserved for certain protocols.
 if(serverPort < 1024 or serverPort > 65535): 
-	print 'Port number must be in the inclusive range [1024, 65535]'
-	exit()
+  print 'Port number must be in the inclusive range [1024, 65535]'
+  exit()
 
 # Check server certificate filename -- make sure this file exists
 if (not os.path.isfile(sys.argv[2])):
-    print "File " + sys.argv[2] + " does not exist"
-    exit()
+  print "File " + sys.argv[2] + " does not exist"
+  exit()
 serverCertPath = sys.argv[2]
 
 # Check server RSA private key filename -- make sure this file exists
 if (not os.path.isfile(sys.argv[3])):
-    print "File " + sys.argv[3] + " does not exist"
-    exit()
+  print "File " + sys.argv[3] + " does not exist"
+  exit()
 serverPrivKeyPath = sys.argv[3]
 
 # Check client certificate filename -- make sure this file exists
 if (not os.path.isfile(sys.argv[4])):
-    print "File " + sys.argv[4] + " does not exist"
-    exit()
+  print "File " + sys.argv[4] + " does not exist"
+  exit()
 clientCertPath = sys.argv[4]
 
 
@@ -58,11 +58,15 @@ clientCertPath = sys.argv[4]
 #====================================================== CTRL+C handler
 
 def signal_handler(signal, frame):
-	print('\nYou pressed Ctrl+C! Exiting...')
-	listenerSocket.close()
-	if userThread.isAlive():
-		userThread.stop()
-	sys.exit(0)
+  print('\nYou pressed Ctrl+C! Exiting...')
+  listenerSocket.close()
+  try:
+    myUserThread
+    if myUserThread.isAlive():
+      myUserThread.stop()
+  except NameError:
+    sys.exit(0)
+  sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
 
 
@@ -70,97 +74,110 @@ signal.signal(signal.SIGINT, signal_handler)
 #====================================================== thread class definition
 
 class userThread(threading.Thread):
-	def __init__(self, _mySocket):
-		threading.Thread.__init__(self)
-		self.mySocket = _mySocket
-		self._stop = threading.Event()
+  def __init__(self, _mySocket):
+    threading.Thread.__init__(self)
+    self.mySocket = _mySocket
+    self._stop = threading.Event()
 
-	#call this function to stop the thread
-	def stop(self):
-		mySocket.close()
-		self._stop.set()
+  #call this function to stop the thread
+  def stop(self):
+    self.mySocket.close()
+    self._stop.set()
 
-	#call this function to check if the thread's "stop" event has been set.
-	def stopped(self):
-		return self._stop.isSet()
+  #call this function to check if the thread's "stop" event has been set.
+  def stopped(self):
+    return self._stop.isSet()
 
-	def run(self):
-		while 1:
-			if(self.stopped()):
-				return
-		
-		receivedMessage = recv_message(mySocket)
-		if receivedMessage is None: #then that means the socket was closed on the client side
-			self.stop()
-			return
+  def run(self):
+    while 1:
+      if(self.stopped()):
+        return
+    
+      receivedMessage = recv_message(self.mySocket)
+      if receivedMessage is None: #then that means the socket was closed on the client side
+        self.stop()
+        return
 
-	unpickled_dict = pickle.loads(receivedMessage)
+      unpickled_dict = pickle.loads(receivedMessage)
 
-    #we established our client-server protocol such that the client's message to the server will always be a dictionary
-    #contanining these 4 fields
-    action = unpickled_dict['action'] #either "get" or "put"
-		filename = unpickled_dict['filename'] #this can be full path + filename, relative path + filename, or just the filename
-		text = unpickled_dict['text'] #this could be plaintext or IV+ciphertext
-		signature = unpickled_dict['signature'] #SHA256 hash, then signed with client's RSA private key
+       #we established our client-server protocol such that the client's message to the server will always be a dictionary
+      #contanining these 4 fields
+      action = unpickled_dict['action'] #either "get" or "put"
+      filename = unpickled_dict['filename'] #this can be full path + filename, relative path + filename, or just the filename
+      text = unpickled_dict['text'] #this could be plaintext or IV+ciphertext
+      signature = unpickled_dict['signature'] #SHA256 hash, then signed with client's RSA private key
 
-		if action == "put":
-			basename = os.path.basename(filename) #if filename is "/foo/bar/text.txt", then basename is "text.txt"
-			try: 
-				f = open(basename, 'wb')
-				f.write(text)
-				f.close()
+      if action != "put" and action != "get":
+        #then the received message was invalid because action must be either "put" or "get". ignore the message.
+        continue
 
-				f = open(basename + '.sha256', 'wb')
-				f.write(signature)
-				f.close()
+      if action == "put":
+        basename = os.path.basename(filename) #if filename is "/foo/bar/text.txt", then basename is "text.txt"
+        try:
+          f = open(basename, 'wb')
+          f.write(text)
+          f.close()
 
-				status = 'success'
-			except IOError as e:
-				#theoretically this shouldn't happen because we are writing the file to the same directory that the server is executing in, so there should be no permission problems
-				status = 'failure'
+          f = open(basename + '.sha256', 'wb')
+          f.write(signature)
+          f.close()
+
+          status = 'success'
+        except IOError as e:
+          #theoretically this shouldn't happen because we are writing the file to the same directory that the server is executing in, so there should be no permission problems
+          status = 'failure'
 
 
 
-			pickled_message = pickle.dumps({
-				'status': status,
-				'text': None, 
-				'signature': None
-				})
-			send_message(mySocket, pickled_message)
+        pickled_message = pickle.dumps({
+          'status': status,
+          'text': None,
+          'signature': None
+          })
+        send_message(self.mySocket, pickled_message)
 
-		if action == "get":
-			try:
-				#filename might be a full or relative path ("/foo/bar/file.txt" or "./subdirectory/file.txt"). the server will then
-				#try to access that. this may fail because we attempt to read something we don't have access to.
-				f = open(filename, 'rb')
-				text = f.read()
-				f.close()
+      if action == "get":
+        try:
+          #filename might be a full or relative path ("/foo/bar/file.txt" or "./subdirectory/file.txt"). the server will then
+          #try to access that. this may fail because we attempt to read something we don't have access to.
+          f = open(filename, 'rb')
+          text = f.read()
+          f.close()
 
-				f = open(filename + '.sha256', 'rb')
-				signature = f.read()
-				f.close()
+          f = open(filename + '.sha256', 'rb')
+          signature = f.read()
+          f.close()
 
-				status = 'success'
-			except IOError as e:
-				status = 'failure'
-				text = None,
-				signature = None
+          status = 'success'
+        except IOError as e:
+          status = 'failure'
+          text = None,
+          signature = None
 
-			pickled_message = pickle.dumps({
-				'status': status,
-				'text': text, 
-				'signature': signature
-				})
-			send_message(mySocket, pickled_message)
+        pickled_message = pickle.dumps({
+          'status': status,
+          'text': text,
+          'signature': signature
+          })
+        send_message(self.mySocket, pickled_message)
 
-		print "Received action '" + action + "', filename '" + filename + "', resulting status is: " + status
-			
+      print "Received action '" + action + "', filename '" + filename + "', resulting status is: " + status
+      
 
 #====================================================== the main code
 
 listenerSocket = socket(AF_INET, SOCK_STREAM)
 listenerSocket.bind(("", serverPort))
 listenerSocket.listen(1)
+#TODO: handle case for when serverPort is already in use:
+#
+# Kevins-MacBook-Pro:ns-group kevinliu$ python server.py 12345 auth/server.crt auth/server.key auth/client.crt 
+# Traceback (most recent call last):
+#   File "server.py", line 166, in <module>
+#     listenerSocket.bind(("", serverPort))
+#   File "/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/socket.py", line 228, in meth
+#     return getattr(self._sock,name)(*args)
+# socket.error: [Errno 48] Address already in use
 
 
 socketToClient, addr = listenerSocket.accept()
@@ -169,5 +186,5 @@ sslSocketToClient = ssl.wrap_socket(socketToClient, server_side=True, certfile=s
 print 'Received incoming connection from ' + addr[0] + ':' + str(addr[1])
 
 #create a thread, give the thread the socket connection with the client, and run the thread
-userThread = userThread(sslSocketToClient)
-userThread.start()
+myUserThread = userThread(sslSocketToClient)
+myUserThread.start()
